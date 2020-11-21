e012de4802e53f4249f4cd4e75d3fe749d3bfbd9
Amit kumar Bharti <61773317+akbharti17@users.noreply.github.com>
2020-11-21 15:14:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+window.onscroll = function() {scrollFunction()};
+
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding="8px 8px";
+    document.getElementById("logo").style.width="0px";
+  } else {
+    document.getElementById("navbar").style.padding="40px 10px";
+    document.getElementById("logo").style.width="30%";
+  }
+}
