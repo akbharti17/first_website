7e7f69625b29c5976451e252e5acd916031de7ee
Your Name <akbharti1717@gmail.com>
2021-07-04 09:47:21 +0530

    final

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 
-window.onscroll = function() {scrollFunction()};
 
 
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("navbar").style.padding="8px 8px";
-    document.getElementById("logo").style.width="0px";
-  } else {
-    document.getElementById("navbar").style.padding="40px 10px";
-    document.getElementById("logo").style.width="30%";
-  }
-}
+// if (window.screen.width < 780) {
+//   window.onscroll = function() {scrollFunction()};
+// }
+
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     document.getElementById("navbar").style.padding="8px 8px";
+//     document.getElementById("logo").style.width="0px";
+//   } else {
+//     document.getElementById("navbar").style.padding="40px 10px";
+//     document.getElementById("logo").style.width="30%";
+//   }
+// }
